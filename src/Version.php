00644425eb66f6f75eca5dc578e70eecc27f6aa6
Jonas Weigert <weigert.jonas@gmail.com>
2023-01-29 10:01:10 -0800

    feat(version): added info(), current(), composer(), package(), and jsonFile() methods

--- src/Version.php
+++ src/Version.php
@@ -2,22 +2,52 @@
 
 namespace CodeUptime\AppVersionLaravel;
 
+use Illuminate\Support\Facades\Log;
+
 class Version
 {
+    public static function info()
+    {
+        return (object)[
+            'composer' => self::composer(),
+            'package' => self::composer(),
+        ];
+    }
+
     public static function current()
     {
-        // check composer.json for version
-        $composerJson = json_decode(file_get_contents(base_path('composer.json')));
-        $composerVersion = data_get($composerJson, 'version');
-        if ($composerVersion) {
-            return $composerVersion;
+        $composer = self::composer();
+        if ($composer) {
+            return $composer;
         }
 
-        // check package.json for version
-        $packageJson = json_decode(file_get_contents(base_path('package.json')));
-        $packageVersion = data_get($composerJson, 'version');
-        if ($packageVersion) {
-            return $packageVersion;
+        $package = self::package();
+        if ($package) {
+            return $package;
+        }
+    }
+
+    public static function composer()
+    {
+        return self::jsonFile(base_path('composer.json'), 'version');
+    }
+
+    public static function package()
+    {
+        return self::jsonFile(base_path('package.json'), 'version');
+    }
+
+    public static function jsonFile(string $path, string $key)
+    {
+        try {
+            // check package.json for version
+            $json = json_decode(file_get_contents($path));
+            $version = data_get($json, $key);
+            if ($version) {
+                return $version;
+            }
+        } catch (\Throwable $e) {
+            Log::debug($e);
         }
     }
 }
