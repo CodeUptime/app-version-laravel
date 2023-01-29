c68a3b5eb9748cab9b21dc0df36b1b536871779d
Jonas Weigert <weigert.jonas@gmail.com>
2023-01-29 10:15:07 -0800

    fix(version): call package helper for version info

--- src/Version.php
+++ src/Version.php
@@ -10,7 +10,7 @@ class Version
     {
         return (object)[
             'composer' => self::composer(),
-            'package' => self::composer(),
+            'package' => self::package(),
         ];
     }
 
